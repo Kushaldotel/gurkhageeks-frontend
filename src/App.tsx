bef386bf6a003810bc3a2ef20a6efca867513a4f
Dinesh Basnet <iamdineshbasnet@gmail.com>
2024-09-05 21:47:47 +0545

    feat: redux toolkit and redux persist config

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
+import { Provider } from "react-redux";
 import "./App.css";
 import ScrollToTop from "./Components/ScrollToTop";
 import Layout from "./Layout";
 import PublicLayout from "./Routes/public-route";
+import { persistor, store } from "./Utils/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 function App() {
   return (
-    <>
-      <Layout>
-        <ScrollToTop />
-        <PublicLayout />
-      </Layout>
-    </>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Layout>
+          <ScrollToTop />
+          <PublicLayout />
+        </Layout>
+      </PersistGate>
+    </Provider>
   );
 }
 
---
+++ src/Utils/store.ts
@@ -0,0 +1,42 @@
+import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import { rootReducer } from "./reducers";
+
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  whitelist: [],
+};
+
+// persisted storage
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  //  ignore all the action types it dispatches:
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+//store redux reducers using redux persist
+const persistor = persistStore(store);
+export { store, persistor };
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
